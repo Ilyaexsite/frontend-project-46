8d06dbd10a69acd82f804f5215ec4e662e9b3656
Ilyaexsite <ilyatsarygin64@gmail.com>
2025-09-04 17:48:54 +0300

    new

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,8 +10,9 @@ program
   .version('1.0.0')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2)
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2, options) => {
+    const diff = genDiff(filepath1, filepath2, options.format)
     console.log(diff)
   })
 
