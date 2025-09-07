7c98ada39713fbe502f3c7510cbd1ee80c81d476
Ilyaexsite <ilyatsarygin64@gmail.com>
2025-09-07 13:39:34 +0300

    new me

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 export default {
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.js$': 'babel-jest',
-    },
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageProvider: 'v8',
-  }
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8'
+}
