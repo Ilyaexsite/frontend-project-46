f155082f8783509989b68b6481d732578e0bf328
Ilyaexsite <ilyatsarygin64@gmail.com>
2025-09-16 18:17:09 +0300

    new nmess

---
+++ src/formatters/stylish.js
@@ -0,0 +1,49 @@
+import _ from 'lodash'
+
+const stringify = (value, depth) => {
+  if (!_.isObject(value)) {
+    if (value === null) return 'null'
+    if (_.isBoolean(value)) return value.toString()
+    return value
+  }
+
+  const indentSize = depth * 4
+  const currentIndent = ' '.repeat(indentSize)
+  const bracketIndent = ' '.repeat(indentSize - 4)
+
+  const lines = Object.entries(value).map(([key, val]) => {
+    const formattedValue = stringify(val, depth + 1)
+    return `${currentIndent}${key}: ${formattedValue}`
+  })
+
+  return ['{', ...lines, `${bracketIndent}}`].join('\n')
+}
+
+const formatStylish = (diff, depth = 1) => {
+  const indentSize = depth * 4
+  const indent = ' '.repeat(indentSize - 2)
+
+  const lines = diff.map((node) => {
+    switch (node.type) {
+      case 'added':
+        return `${indent}+ ${node.key}: ${stringify(node.value, depth + 1)}`
+      case 'removed':
+        return `${indent}- ${node.key}: ${stringify(node.value, depth + 1)}`
+      case 'unchanged':
+        return `${indent}  ${node.key}: ${stringify(node.value, depth + 1)}`
+      case 'changed':
+        return [
+          `${indent}- ${node.key}: ${stringify(node.oldValue, depth + 1)}`,
+          `${indent}+ ${node.key}: ${stringify(node.newValue, depth + 1)}`,
+        ].join('\n')
+      case 'nested':
+        return `${indent}  ${node.key}: ${formatStylish(node.children, depth + 1)}`
+      default:
+        throw new Error(`Unknown node type: ${node.type}`)
+    }
+  })
+
+  return ['{', ...lines, `${' '.repeat(indentSize - 4)}}`].join('\n')
+}
+
+export default formatStylish
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,67 +1,15 @@
-import _ from 'lodash'
+import buildDiff from './diffBuilder.js'
+import formatStylish from './formatters/stylish.js'
 import parseFile from './parsers.js'
-
-const buildDiff = (data1, data2) => {
-  const allKeys = _.union(_.keys(data1), _.keys(data2))
-  const sortedKeys = _.sortBy(allKeys)
-
-  return sortedKeys.flatMap((key) => {
-    const hasInFirst = _.has(data1, key)
-    const hasInSecond = _.has(data2, key)
-    const value1 = data1[key]
-    const value2 = data2[key]
-
-    if (!hasInFirst) {
-      return [{ key, value: value2, status: 'added' }]
-    }
-
-    if (!hasInSecond) {
-      return [{ key, value: value1, status: 'removed' }]
-    }
-
-    if (_.isEqual(value1, value2)) {
-      return [{ key, value: value1, status: 'unchanged' }]
-    }
-
-    return [
-      { key, value: value1, status: 'removed' },
-      { key, value: value2, status: 'added' }
-    ]
-  })
-}
-
-const formatValue = (value) => {
-  if (_.isBoolean(value)) {
-    return value.toString()
-  }
-  if (_.isNull(value)) {
-    return 'null';
-  }
-  if (_.isObject(value)) {
-    return '[complex value]'
-  }
-  return value
-}
-
-const formatStylish = (diffItems) => {
-  const lines = diffItems.map((item) => {
-    const prefix = item.status === 'added' ? '  + ' :
-      item.status === 'removed' ? '  - ' :
-        '    '
-    return `${prefix}${item.key}: ${formatValue(item.value)}`
-  })
-
-  return `{\n${lines.join('\n')}\n}`
-}
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const data1 = parseFile(filepath1)
   const data2 = parseFile(filepath2)
 
-  const diffItems = buildDiff(data1, data2)
+  const diff = buildDiff(data1, data2)
 
   if (format === 'stylish') {
-    return formatStylish(diffItems)
+    return formatStylish(diff)
   }
 
   throw new Error(`Unsupported format: ${format}`)
