8916f87a1786e21d37d4847ea88c8c36b428d8f1
Ilyaexsite <ilyatsarygin64@gmail.com>
2025-09-16 19:37:00 +0300

    go

---
+++ src/formatters/stylish.js
@@ -0,0 +1,43 @@
+import _ from 'lodash'
+
+const formatValue = (value, depth = 0) => {
+  if (_.isBoolean(value)) {
+    return value.toString()
+  }
+  if (_.isNull(value)) {
+    return 'null'
+  }
+  if (!_.isPlainObject(value)) {
+    return value
+  }
+
+  const indent = '  '.repeat(depth * 2)
+  const lines = Object.entries(value).map(([key, val]) => {
+    const formattedValue = formatValue(val, depth + 1)
+    return `${indent}  ${key}: ${formattedValue}`
+  })
+
+  return `{\n${lines.join('\n')}\n${indent}}`
+}
+
+const formatStylish = (diff, depth = 0) => {
+  const indent = '  '.repeat(depth * 2)
+  const lines = diff.map((item) => {
+    const { key, status } = item
+
+    if (status === 'nested') {
+      const nestedContent = formatStylish(item.children, depth + 1)
+      return `${indent}  ${key}: ${nestedContent}`
+    }
+
+    const value = formatValue(item.value, depth)
+    const prefix = status === 'added' ? '+ ' :
+      status === 'removed' ? '- ' : '  '
+
+    return `${indent}${prefix}${key}: ${value}`
+  })
+
+  return `{\n${lines.join('\n')}\n${indent}}`
+}
+
+export default formatStylish
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,43 +1,6 @@
 import _ from 'lodash'
 import parseFile from './parsers.js'
-
-const formatValue = (value, depth = 0) => {
-  if (_.isBoolean(value)) {
-    return value.toString()
-  }
-  if (_.isNull(value)) {
-    return 'null'
-  }
-  if (!_.isPlainObject(value)) {
-    return value
-  }
-
-  const indent = '    '.repeat(depth)
-  const lines = Object.entries(value).map(([key, val]) => {
-    const formattedValue = formatValue(val, depth + 1)
-    return `${indent}    ${key}: ${formattedValue}`
-  })
-
-  return `{\n${lines.join('\n')}\n${indent}}`
-}
-
-const formatStylish = (diff, depth = 0) => {
-  const indent = '    '.repeat(depth)
-  const lines = diff.map((item) => {
-    const { key, status } = item
-    if (status === 'nested') {
-      const nestedContent = formatStylish(item.children, depth + 1)
-      return `${indent}    ${key}: ${nestedContent}`
-    }
-    const value = formatValue(item.value, depth)
-    const prefix = status === 'added' ? '  + ' :
-      status === 'removed' ? '  - ' : '    '
-
-    return `${indent}${prefix}${key}: ${value}`
-  })
-
-  return `{\n${lines.join('\n')}\n${indent}}`
-}
+import formatStylish from './formatters/stylish.js'
 
 const buildDiff = (data1, data2) => {
   const allKeys = _.union(_.keys(data1), _.keys(data2))
