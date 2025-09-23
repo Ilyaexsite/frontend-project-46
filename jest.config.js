1db6b6da9d92b2008e59a2474c1dfc0ce9f901f6
Ilyaexsite <ilyatsarygin64@gmail.com>
2025-09-23 15:03:42 +0300

    end

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 export default {
   testEnvironment: 'node',
   collectCoverageFrom: [
-    'src/**/*.js',
+    'src/**/*.js'
   ],
   transform: {},
   extensionsToTreatAsEsm: ['.js'],
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  }
 }
